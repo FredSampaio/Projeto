c2972a4f3cd7ea09522f44072e3b0f6ab0796646
lmmf <leonardo.mmferreira@gmail.com>
2017-06-07 22:04:13 -0300

    Animais agora são inseridos por JS também.

---
+++ User/animais.js
@@ -0,0 +1,36 @@
+﻿let dados_animais=`[
+    {"url":"Animais/fifi.jpg", "nome":"Fifi", "id":1, "raca":"Poodle", "idade":3},
+	{"url":"Animais/rex.jpg", "nome":"Rex", "id":2, "raca":"Pit Bull", "idade":2},
+	{"url":"Animais/pantera.jpg", "nome":"Pantera", "id":3, "raca":"Bengal", "idade":1},
+	{"url":"Animais/lorde.jpg", "nome":"Lorde", "id":4, "raca":"Persa", "idade":6},
+	{"url":"Animais/cenoura.jpg", "nome":"Cenoura", "id":5, "raca":"Coelho", "idade":3},
+	{"url":"Animais/faisca.jpg", "nome":"Faísca", "id":6, "raca":"Calopsita", "idade":2},
+    {"url":"Animais/haroldo.jpg", "nome":"Haroldo", "id":7, "raca":"Furão", "idade":8}
+]`;
+
+let animais=JSON.parse(dados_animais);
+
+for(let i=0;i<animais.length;i++){
+    let obj=animais[i];	
+	document.getElementById("animais").innerHTML+='\
+						<div class="animal">\
+							<img src="'+obj.url+'" alt="'+obj.nome+'" style="width:160px;height:160px">\
+						</div>\
+						<div class="quantidade">\
+							<p>Nome:</p>\
+							<p>'+obj.nome+'</p>\
+						</div>\
+						<div class="quantidade">\
+							<p>ID:</p>\
+							<p>'+obj.id+'</p>\
+						</div>\
+						<div class="quantidade">\
+							<p>Raça:</p>\
+							<p>'+obj.raca+'</p>\
+						</div>\
+						<div class="quantidade">\
+							<p>Idade:</p>\
+							<p>'+obj.idade+' anos</p>\
+						</div>\
+						<br>';
+}
